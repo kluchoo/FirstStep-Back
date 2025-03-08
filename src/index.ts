84e8e0dfa946d37598db2c781cf2cab3910d2589
kluchoo <dominikzucho@gmail.com>
2025-03-08 21:40:49 +0100

    Instalacja i implementacja swagera

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,99 @@
 import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+//swager opcje
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/index.ts'], // Ścieżka do plików z dokumentacją
+};
+
+const specs = swaggerJsdoc(options);
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 
 app.get('/api/users', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
 
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     summary: Hello World
+ *     responses:
+ *       200:
+ *         description: Hello World
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: number
+ *                 email:
+ *                   type: string
+ */
 app.post('/api/users', async (req, res) => {
   const { email } = req.body;
   const user = await prisma.user.create({
